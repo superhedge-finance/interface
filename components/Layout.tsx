f20c105a51a98e5e291c3d7e02fc8f9b7cc310f0
mhtsakok <chris@superhedge.com>
2025-04-13 16:07:51 +0800

    feat: add Google Tag Manager with environment variable support

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Head from "next/head";
 import Header from "./header/Header";
 import Footer from "./footer/Footer";
+import { GoogleTagManager } from '@next/third-parties/google';
 
 type LayoutProps = {
   children?: React.ReactNode;
@@ -16,6 +17,7 @@ const Layout = ({ children }: LayoutProps) => {
       </Head>
       <main className='w-full bg-[#F7F7F7]'>
         <Header />
+        <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_MEASUREMENT_ID ?? ''} />
         <div className='container mx-auto min-h-[calc(100vh-210px)] p-5 md:p-8'>
           {children}
         </div>
